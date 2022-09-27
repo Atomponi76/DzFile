8ca01db5bc96bc8dc07a459407511f45cdc4df36
Atomponi76 <atomponi76@gmail.com>
2022-09-27 12:12:00 +0300

    Init

---
+++ File.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class File
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int SizeBytes { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string ConverSize {
+            get
+            {
+                string[] sizeType = new string[] { "b","kb","mb","gb"};
+                int a=0;
+                for(int i = 0; SizeBytes > 1024;)
+                {
+                    i++;
+                    SizeBytes /= 1024;
+                    a = i;
+                }
+                return $"{SizeBytes} {sizeType[a]}";
+            }
+        }
+        public File()
+        {
+
+        }
+        public File(string name, string type, int sizeBytes, DateTime createDate)
+        {
+            Name = name;
+            Type = type;
+            SizeBytes = sizeBytes;
+            CreateDate = createDate;
+        }
+
+        public override string ToString()
+        {
+            return $" Name : {Name}{Type} with size {ConverSize}, created date: {CreateDate}";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using ConsoleApp11;
+using File = ConsoleApp11.File;
+
+File[] files = new File[] {
+
+  new File("KK", ".txt",1024, new DateTime(2018, 7,30)),
+  new File("Forhe", ".txt", 4578954, DateTime.Now),
+  new File("Forhua", ".exe", 457895986, DateTime.Now),
+  new File("Koker", ".txt", 10335, DateTime.Now),
+};
+foreach(File file in files)
+{
+    if(file.Type == ".txt")
+    {
+        Console.WriteLine(file.ToString());
+    }
+}
+foreach(File file in files)
+{
+    if (file.Name.Contains("ua"))
+    {
+        Console.WriteLine(file.ToString());
+    }
+}
+foreach(File file in files)
+{
+    if(file.CreateDate < new DateTime(2022, 9, 17))
+    {
+        Console.WriteLine(file.ToString());
+    }
+}
